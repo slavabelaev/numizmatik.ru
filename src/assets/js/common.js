22748add783d65a009599fe7cabf25aebd06bda4
belaevslava <belaevslava@gmail.com>
2018-03-15 19:36:49 +0200

    numizmatic

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -1,3 +1,20 @@
+'use strict';
+
 window.onload = function() {
+	var animationDuration = 200;
+	
+	if (window.innerWidth > 768) {
+		document.querySelectorAll('.list-of-figures').forEach(function(listOfFiguresElement) {
+			listOfFiguresElement.querySelectorAll('li').forEach(function(itemElement, index) {
+				var currentItemAnimationDelay = animationDuration * index;
+
+				itemElement
+					.setAttribute('data-aos', 'fade-up');
+				itemElement
+					.setAttribute('data-aos-delay', currentItemAnimationDelay);
+			});
+		});
+	}
+
 	AOS.init();
 }
